8c55d01644e43362ce7762fc52e9fc387dda82d7
uchenoel <noeluchechukwu@gmail.com>
2021-04-09 08:23:30 +0100

    litle change

---
+++ src/components/Bio.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Bio() {
+  return (
+    <div
+      style={{
+        display: 'flex',
+      }}
+    >
+      <p style={{ maxWidth: 320 }}>Personal Code book by Uchechukwu Noel</p>
+    </div>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,49 +4,13 @@ import { getAllPosts } from '../lib/api';
 import Image from 'next/image';
 import styles from './index.module.css';
 import ReactTimeAgo from 'react-time-ago';
+import Layout from '../components/Layout';
 
 export default function Home({ allPosts }) {
   return (
-    <div className={styles.container}>
-      <header className={styles.header}>
-        <Head>
-          <title>My page title</title>
-          <meta
-            name='viewport'
-            content='initial-scale=1.0, width=device-width'
-          />
-        </Head>
-
-        <nav className={styles.nav}>
-          <Image
-            src='/logolight.svg'
-            alt='Picture of the author'
-            width={200}
-            height={100}
-          />
-        </nav>
-      </header>
-
-      <main>
-        <section className={styles.posts}>
-          <ul className={styles.sessions}>
-            {allPosts &&
-              allPosts.map((post) => {
-                return (
-                  <li className={styles.post} key={post.slug}>
-                    <div className={styles.time}>{post.date}</div>
-                    <Link as={`/posts/${post.slug}`} href='/posts/[slug]'>
-                      <a className={styles.title}>{post.title}</a>
-                    </Link>
-                  </li>
-                );
-              })}
-          </ul>
-        </section>
-      </main>
-
-      <footer></footer>
-    </div>
+    <Layout>
+      <p>the beginning of something great!</p>
+    </Layout>
   );
 }
 
