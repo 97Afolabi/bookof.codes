5ac7a354cf46f435fc7fadc6b16a0f262948242a
uchenoel <noeluchechukwu@gmail.com>
2021-04-09 19:13:18 +0100

    style v0.1

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -10,13 +10,8 @@ export default function Layout({ children }) {
           name='description'
           content='Learn how to build a personal website using Next.js'
         />
-        <meta
-          property='og:image'
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
-        <meta name='og:title' content={siteTitle} />
+
+        <meta name='og:title' content='bookof.codes' />
         <meta name='twitter:card' content='summary_large_image' />
       </Head>
       <header></header>
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,97 @@ import Head from 'next/head';
 import Link from 'next/link';
 import { getAllPosts } from '../lib/api';
 import Image from 'next/image';
-import styles from './index.module.css';
-import ReactTimeAgo from 'react-time-ago';
 import Layout from '../components/Layout';
 
 export default function Home({ allPosts }) {
   return (
-    <Layout>
-      <p>the beginning of something great!</p>
-    </Layout>
+    <div className='container'>
+      <header className='main-header'>
+        <Link href='/'>
+          <a>
+            <h1>bookofcodes</h1>
+          </a>
+        </Link>
+      </header>
+
+      <aside className='intro-card'>
+        <div className='bio-card'>
+          <p>
+            Personal code book by
+            <Link href='https://twitter.com/cybernuel'>
+              <a>Uchechukwu Noel.</a>
+            </Link>
+          </p>
+        </div>
+      </aside>
+
+      <main>
+        <article>
+          <header>
+            <Link href='/'>
+              <a>
+                <h2>Cuncurrent Go!</h2>
+              </a>
+            </Link>
+
+            <small>Feb 23, 2021</small>
+          </header>
+          <p>What i learnt from working with go.</p>
+        </article>
+
+        <article>
+          <header>
+            <Link href='/'>
+              <a>
+                <h2>Cuncurrent Go! Yes!</h2>
+              </a>
+            </Link>
+
+            <small>Feb 23, 2021</small>
+          </header>
+          <p>What i learnt from working with go.</p>
+        </article>
+
+        <article>
+          <header>
+            <Link href='/'>
+              <a>
+                <h2>Cuncurrent Go! You should try it</h2>
+              </a>
+            </Link>
+
+            <small>Feb 23, 2021</small>
+          </header>
+          <p>What i learnt from working with go.</p>
+        </article>
+
+        <article>
+          <header>
+            <Link href='/'>
+              <a>
+                <h2>Cuncurrent Go! Why not?</h2>
+              </a>
+            </Link>
+
+            <small>Feb 23, 2021</small>
+          </header>
+          <p>What i learnt from working with go.</p>
+        </article>
+      </main>
+      <footer>
+        <Link href=''>
+          <a>github</a>
+        </Link>
+
+        <Link href=''>
+          <a>twitter</a>
+        </Link>
+
+        <Link href=''>
+          <a>rss</a>
+        </Link>
+      </footer>
+    </div>
   );
 }
 
