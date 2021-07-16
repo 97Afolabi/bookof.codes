02de207e79840417406d35574a163f4723eb5c9a
onyesoftware <noeluchechukwu@gmail.com>
2021-07-16 12:49:31 +0100

    new white theme

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,24 +6,72 @@ import { getPosts } from '../lib/api';
 
 export default function Home({ allPosts }) {
   return (
-    <Layout pageTitle='Home' description='Personal Code  Blog By Uchechukwu'>
-      <Bio />
-      {allPosts.map((post) => {
-        return (
-          <article key={post.title} className='mb-16'>
-            <header>
-              <h3 className='font-heads font-black text-2xl leading-7 dark:text-tomato'>
-                <Link href={`/posts/${post.slug}`}>
-                  <a>{post.title}</a>
-                </Link>
-              </h3>
-              <small className='dark:text-mgrey'>{post.date}</small>
-            </header>
-            <p className='font-body dark:text-mgrey'>{post.excerpt}</p>
+    <div className='container'>
+      <header>
+        <h1>bookof.codes</h1>
+        <nav>
+          <Link href='/'>
+            <a className='subtn'>subscribe</a>
+          </Link>
+          <Link href='/'>
+            <a>twitter</a>
+          </Link>
+          <Link href='/'>
+            <a>rss</a>
+          </Link>
+        </nav>
+      </header>
+      <main>
+        <div className='intro'>
+          <h1>About</h1>
+          <p>
+            This site is a collection of things i’ve had to search on google and
+            stackoverflow over the course of my career so it’s more like a
+            memory cache of “How To”. I hope you what you need to help you
+            figure out whatever it is that lead you here. For any sugestions or
+            enquiries you can reach me on twitter or checkout my personal site.
+          </p>
+        </div>
+
+        <div className='widget'>
+          <input type='text' />
+        </div>
+
+        <section>
+          <article>
+            <div id='date-tag'>20-20-29</div>
+            <div id='post-card'>
+              <h2>Serving Static Files in golang </h2>
+              <p>How to serve static files in go web servers.</p>
+            </div>
           </article>
-        );
-      })}
-    </Layout>
+
+          <article>
+            <div id='date-tag'>20-20-29</div>
+            <div id='post-card'>
+              <h2>Serving Static Files in golang </h2>
+              <p>How to serve static files in go web servers.</p>
+            </div>
+          </article>
+
+          <article>
+            <div id='date-tag'>20-20-29</div>
+            <div id='post-card'>
+              <h2>Serving Static Files in golang </h2>
+              <p>How to serve static files in go web servers.</p>
+            </div>
+          </article>
+
+          <article>
+            <div id='date-tag'>20-20-29</div>
+            <div id='post-card'>
+              <h2>Serving Static Files in golang </h2>
+              <p>How to serve static files in go web servers.</p>
+            </div>
+          </article>
+        </section>
+      </main>
+    </div>
   );
 }
 
